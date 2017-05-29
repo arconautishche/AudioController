bdab986db8571c31a957e547c35d64ac973a7f8d
arconautishche <arcoant@gmail.com>
2017-05-29 21:32:04 +0200

    First http request

--- AudioControllerApp/app/src/main/java/com/example/anton/audiocontrollerapp/MainActivity.java
+++ AudioControllerApp/app/src/main/java/com/example/anton/audiocontrollerapp/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.anton.audiocontrollerapp;
 
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Toast;
+
         import java.io.IOException;
         import java.io.InputStream;
         import java.io.InputStreamReader;
@@ -7,9 +12,14 @@ package com.example.anton.audiocontrollerapp;
         import java.net.MalformedURLException;
         import java.net.URL;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
+        import com.android.volley.Request;
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.StringRequest;
+        import com.android.volley.toolbox.Volley;
+
+        import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,30 +32,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View view) {
 
-        URL url;
-        HttpURLConnection urlConnection = null;
-        try {
-            url = new URL("http://192.168.1.43:8080/AudioController/api/v1/controller/zones/1");
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
 
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-
-            InputStream in = urlConnection.getInputStream();
-
-            InputStreamReader isw = new InputStreamReader(in);
-
-            int data = isw.read();
-            while (data != -1) {
-                char current = (char) data;
-                data = isw.read();
-                System.out.print(current);
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText(getApplicationContext(), "Response is: "+ response.substring(0,50), Toast.LENGTH_LONG).show();
+                        //JSONObject
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Response is: "+ error.toString(), Toast.LENGTH_LONG).show();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
