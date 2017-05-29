bba7d99588e4597e254cb9792352b53d2ce06c98
arconautishche <arcoant@gmail.com>
2017-05-29 20:38:26 +0200

    added Volley

--- AudioControllerApp/app/src/main/java/com/example/anton/audiocontrollerapp/MainActivity.java
+++ AudioControllerApp/app/src/main/java/com/example/anton/audiocontrollerapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.anton.audiocontrollerapp;
 
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButtonClick(View view) {
+
+        URL url;
+        HttpURLConnection urlConnection = null;
+        try {
+            url = new URL("http://192.168.1.43:8080/AudioController/api/v1/controller/zones/1");
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+
+            InputStream in = urlConnection.getInputStream();
+
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+                System.out.print(current);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
     }
 }
