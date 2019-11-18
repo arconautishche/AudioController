9ef698f19cb0c0eb0613d5e0dda6cef75b6d6043
quadu <wim.welkenhuyzen@gmail.com>
2019-11-18 16:02:58 +0100

    moooore logging

--- Server/AudioController.py
+++ Server/AudioController.py
@@ -127,7 +127,10 @@ class AudioController:
         self._gpio.output(zone.bcm, self._gpio.LOW if enabled else self._gpio.HIGH)
 
     def _send_volumes(self):
+        log(DEBUG, "zones:".format(self.zones))
+        log(DEBUG, "zones values:".format(self.zones.values()))
         volumes = [zone.volume for zone in self.zones.values()]
+        log(DEBUG, "zones volumes:".format(volumes))
         normalized_volumes = [int(256 * vol / self.MAX_VOLUME) for vol in volumes]
         log(DEBUG, "Normalized volumes:".format(normalized_volumes))
         data = bytes(normalized_volumes)
